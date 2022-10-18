bb4d593d31470940aa5ff271a4e3d820a6f7ff86
Vandan Parmar <vandan.parmar@datarobot.com>
2022-10-18 13:58:10 +0100

    alternative to fix int64 issue

--- winequality/custom.py
+++ winequality/custom.py
@@ -6,14 +6,10 @@ def load_model(code_dir):
     model = mlflow.pyfunc.load_model(code_dir)
     return model
 
-def read_input_data(input_binary_data):
-    import io
-    df_scoring = pd.read_csv(io.BytesIO(input_binary_data))
-    df_scoring['is_red'] = df_scoring['is_red'].astype(np.int64)
-    return df_scoring
 
 # for binary classification use case:
 def score(data, model, **kwargs):
+    data['is_red'] = data['is_red'].astype(np.int64)
     predictions = model.predict(data)
     df_predictions = pd.DataFrame(data=predictions, columns=['True'])
     df_predictions['False'] = 1-df_predictions['True']
