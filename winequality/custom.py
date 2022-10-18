1758af51340015c0498f9caba9e1cf39ea4b92e3
Vandan Parmar <vandan.parmar@datarobot.com>
2022-10-18 13:49:23 +0100

    added read input hook to stop int64 type clashing

--- winequality/custom.py
+++ winequality/custom.py
@@ -1,9 +1,16 @@
 import mlflow
 import pandas as pd
+import numpy as np
 
 def load_model(code_dir):
     model = mlflow.pyfunc.load_model(code_dir)
     return model
+
+def read_input_data(input_binary_data):
+    import io
+    df_scoring = pd.read_csv(io.BytesIO(input_binary_data))
+    df_scoring['is_red'] = df_scoring['is_red'].astype(np.int64)
+    return df_scoring
 
 # for binary classification use case:
 def score(data, model, **kwargs):
