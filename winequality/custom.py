8cba2782fbfd47d376479e77631ef47b06791956
Vandan Parmar <vandan.parmar@datarobot.com>
2022-10-18 12:13:51 +0100

    added score method for regression

--- winequality/custom.py
+++ winequality/custom.py
@@ -5,9 +5,16 @@ def load_model(code_dir):
     model = mlflow.pyfunc.load_model(code_dir)
     return model
 
-
+# for binary classification use case:
 def score(data, model, **kwargs):
     predictions = model.predict(data)
     df_predictions = pd.DataFrame(data=predictions, columns=['True'])
     df_predictions['False'] = 1-df_predictions['True']
     return df_predictions
+
+
+# # for regression use case:
+# def score(data, model, **kwargs):
+#     predictions = model.predict(data)
+#     df_predictions = pd.DataFrame(data=predictions, columns=['Predictions'])
+#     return df_predictions
